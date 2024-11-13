e506096fd639252fbc0d92d2258e9d94d5d945ce
Lucas Paes <lucas@Mac-Pro-de-Lucas.local>
2024-11-13 00:15:34 -0300

    feature

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { MainRouter } from "./router/router.tsx";
 
-
-import "./styles/global.css"
-import "./styles/reset.css"
 import { ReactQueryInstanceClient } from "./components/layouts/react-query-instance-client/react-query-instance-client.tsx";
 
-import 'react-toastify/dist/ReactToastify.css';
-
 import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+import "./styles/global.css"
+import "./styles/reset.css"
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ReactQueryInstanceClient >
