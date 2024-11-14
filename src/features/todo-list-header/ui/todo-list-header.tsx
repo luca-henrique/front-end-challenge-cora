64781431019c73eb06f9078222326e84c86f65f0
Lucas Paes <lucas@Mac-Pro-de-Lucas.local>
2024-11-14 00:18:56 -0300

    refactor: header todo list

--- src/features/todo-list-header/ui/todo-list-header.tsx
+++ src/features/todo-list-header/ui/todo-list-header.tsx
@@ -1,7 +1,27 @@
-import { HeaderTodo } from "../../../components/molecules/header-todo/header-todo"
+
+import logoImage from "../../../assets/images/logo.svg";
+
+import './style.css'
 
 export const TodoListHeader = () => {
   return (
-    <HeaderTodo />
+    <header className="todo-list-header">
+      <img src={logoImage} alt="Cora" title="Cora"></img>
+      <h1>Weekly to-do list &#128467;</h1>
+      <h2>
+        Bem-vindo ao nosso produto <i>fake</i> de <strong>to-do</strong> list
+      </h2>
+      <p>
+        Marque como{" "}
+        <strong>
+          <u>done</u>
+        </strong>{" "}
+        as tasks que você conseguir concluir (elas já precisam renderizar com
+        o status <strong>done</strong>)
+      </p>
+      <p className="disclaimer">
+        Items obrigatórios marcados com arteristico (<strong>*</strong>)
+      </p>
+    </header>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "./app/styles/global.css"
 import "./app/styles/reset.css"
+
 import { AppProviders } from "./app/provider/index.tsx";
 
 
